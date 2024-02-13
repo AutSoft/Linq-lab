4b2152af34e7c36258c51e08cab253423afee1e0
Tóth Tibor <toth.tibor@autsoft.hu>
2024-02-13 16:22:13 +0100

    Add project files.

---
+++ HelloLinq/Extensions/StringExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace HelloLinq.Extensions;
+
+public static class StringExtensions
+{
+    public static string TrimPad<T>(this T obj, int length)
+        => ((obj?.ToString() ?? string.Empty) + new string(' ', length)).Substring(0, length);
+}
---
+++ HelloLinq/Program.cs
@@ -0,0 +1,35 @@
+﻿
+using HelloLinq;
+using System;
+using System.Collections.Generic;
+
+static List<Dog> ListDogsByNamePrefix(IEnumerable<Dog> dogs,
+    string prefix)
+{
+    var result = new List<Dog>();
+    foreach (var dog in dogs)
+    {
+        if (dog.Name.StartsWith(prefix,
+            StringComparison.OrdinalIgnoreCase))
+            result.Add(dog);
+    }
+    return result;
+}
+
+static string GetInput()
+{
+    Console.Write("Írd be a kutya nevének elejét: ");
+    var result = Console.ReadLine();
+    Console.Clear();
+    return result;
+}
+
+string searchText;
+IEnumerable<Dog> Dogs = Dog.Repository.Values;
+while ((searchText = GetInput()).Length > 0)
+{
+    foreach (var dog in ListDogsByNamePrefix(Dogs, searchText))
+        Console.WriteLine(dog);
+}
+
+
